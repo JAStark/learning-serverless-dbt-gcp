41cd9de17c1f8072062487e92265346271ddec50
Jennifer Stark <jastark1@gmail.com>
2022-06-11 15:33:20 +0100

    add missing arg in tf

--- environments/dev/terraform.tf
+++ environments/dev/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
-    google = {
-      source = "hashicorp/google"
+    google    = {
+      source  = "hashicorp/google"
       version = "~>4.22.0"
     }
   }
@@ -29,25 +29,25 @@ resource "google_artifact_registry_repository" "dev-serverless-dbt-repo"     {
 }
 
 resource "google_service_account" "dbt_serverless_workflow_account" {
-  account_id = "workflows-demos-account"
-  display_name = "DBT Workflows Demo Account"
+  account_id    = "workflows-demos-account"
+  display_name  = "DBT Workflows Demo Account"
 }
 
 resource "google_workflows_workflow" "dbt_demo_workflow" {
-  name = "dbt_serverless_workflow_demo"
-  region = "europe-west1"
-  description = "demo workflow for cloud run, and dbt w/ snowflake"
+  name            = "dbt_serverless_workflow_demo"
+  region          = "europe-west1"
+  description     = "demo workflow for cloud run, and dbt w/ snowflake"
   service_account = google_service_account.dbt_serverless_workflow_account.id
   source_contents = file("workflow.yaml")
 }
 
 
 resource "google_cloud_scheduler_job" "dbt-workflows-job" {
-  name = "dbt-serverless-workflows-job"
-  description = "trigger the workflow once per day"
-  schedule = "0 9 * * *"
-  time_zone = "Europe/London"
-  attempt_deadline = "320s"
+  name              = "dbt-serverless-workflows-job"
+  description       = "trigger the workflow once per day"
+  schedule          = "0 9 * * *"
+  time_zone         = "Europe/London"
+  attempt_deadline  = "320s"
 
   retry_config {
     retry_count = 1
@@ -56,6 +56,6 @@ resource "google_cloud_scheduler_job" "dbt-workflows-job" {
   http_target {
     http_method = "POST"
     # uri = "https://workflowexecutions.googleapis.com/v1/projects/${PROJECT_ID}/locations/europe-west1/workflows/dbt_serverless_workflow_demo/executions"
-    google_workflows_workflow.dbt_demo_workflow.id
+    uri         = google_workflows_workflow.dbt_demo_workflow.id
   }
 }
