4cceccd6862da5dd0a1b4d284ab1c26132a0fd19
Jennifer Stark <jastark1@gmail.com>
2022-06-12 16:57:44 +0100

    tweaked dockerfile and main; new port

--- dbt_cloud_run_1/main.py
+++ dbt_cloud_run_1/main.py
@@ -1,9 +1,11 @@
+import logging
 import os
 
 from flask import Flask
 
 app = Flask(__name__)
 
+logging.getLogger().setLevel(logging.INFO)
 
 @app.route("/")
 def hello_world():
@@ -13,16 +15,16 @@ def hello_world():
     #     return "SQL Transformations complete"
     # except Exception as e:
     #     print("\nError = {e}")
-    #     return "error!!!"
-
+    #     return "error!"
+    logging.info("running hello_world…")
     return "test cloud run main complete. No DBT used."
 
 
 if __name__ == "__main__":
     # Redirect Flask logs to Gunicorm logs
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
-    app.logger.info('Service started…')
-else:
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+#     gunicorn_logger = logging.getLogger('gunicorn.error')
+#     app.logger.handlers = gunicorn_logger.handlers
+#     app.logger.setLevel(gunicorn_logger.level)
+#     app.logger.info('Service started…')
+# else:
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8089)))
