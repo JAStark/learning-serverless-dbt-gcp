3a80b8dda52a747e5ef750a23df6f3fef1b25c5c
Jennifer Stark <jastark1@gmail.com>
2022-06-12 17:28:35 +0100

    made service account email a var

--- environments/dev/terraform.tf
+++ environments/dev/terraform.tf
@@ -93,7 +93,7 @@ resource "google_cloud_scheduler_job" "dev-dbt-workflows-job" {
     uri         = "https://workflowexecutions.googleapis.com/v1/projects/${PROJECT_ID}/locations/europe-west1/workflows/dev_dbt_serverless_workflow_demo/executions"
 
     oauth_token {
-      service_account_email = data.google_service_account.dbt_serverless_workflow_account.email
+      service_account_email = var.service_account_email
     }
   }
 }
